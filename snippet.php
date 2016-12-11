41548b720aed3d8ff33a444d3d03e1e342e07d8b
Igor Sukhinin <suhinin@bdcolors.com>
2016-12-12 01:00:49 +0200

    Totally refactored the snippet code

--- snippet.php
+++ snippet.php
@@ -2,100 +2,327 @@
 
 /*
  *
- * Created by: Igor Sukhinin, Baltic Design Colors Ltd
- * License: GNU General Public License v3.0
+ * @author Igor Sukhinin <suhinin@gmail.com>, Baltic Design Colors Ltd
+ * @license: https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * @version: 1.1.0
  *
- */
-
-/*
  * Variables
  * ---------
  * @var modX $modx
  *
  * Properties
  * ----------
- * @property    string     accountName
- * @property    integer    limit
- * @property    integer    showVideo
- * @property    string     imageQuality
- * @property    string     videoQuality
+ * @property    string      $accountName    Instagram account name; required
+ * @property    integer     $limit          Limit on the maximum number of items that will be displayed
+ * @property    integer     $showVideo      Show the video if it's available (options: 1 | 0)
+ * @property    string      $imageQuality   Image quality (options: low_resolution | thumbnail | standard_resolution)
+ * @property    string      $videoQuality   Video quality (options: low_resolution | standard_resolution | low_bandwidth)
+ * @property    string      $innerTpl       Inner chunk name
+ * @property    string      $outerTpl       Outer chunk name
+ * @property    string      $errorTpl       Error chunk name
  *
  */
 
-/*
- * Initialize essential properties
-*/
 
-// Set the Instagram account name
-if (!isset($accountName)) {
-    return 'Please set the Instagram account name!';
-}
+class InstagramLatestPosts
+{
+    protected $accountName;
+    protected $limit;
+    protected $showVideo;
+    protected $imageQuality;
+    protected $videoQuality;
+    protected $innerTpl;
+    protected $outerTpl;
+    protected $errorTpl;
+    protected $modx;
+    protected $serverMethod;
+    protected $accountUrl;
+    protected $output;
+    protected $error;
 
-// Set the limit on the maximum number of items that will be displayed
-$limit = (isset($limit)) ? $limit : 6;
-
-// Do we need to show the video as well?
-// Available options: 1 | 0
-$showVideo = (isset($showVideo)) ? $showVideo : 0;
-
-// Set the image quality
-// Available options: low_resolution | thumbnail | standard_resolution
-$imageQuality = (isset($imageQuality)) ? $imageQuality : 'low_resolution';
-
-// Set the video quality
-// Available options: low_resolution | standard_resolution | low_bandwidth
-$videoQuality = (isset($videoQuality)) ? $videoQuality : 'low_resolution';
-
-// Set the account URL
-$accountUrl = 'https://www.instagram.com/' . $accountName;
-
-// Set the JSON URL
-$jsonUrl = $accountUrl . '/media/';
-
-// Get the JSON content
-$jsonData = file_get_contents($jsonUrl);
-
-// Decode the JSON in object
-$data = json_decode($jsonData);
-
-// Prepare the variables
-$resources = [];
-$i = 0;
-$output = '';
-$items = '';
-
-foreach ($data->items as $item) {
-    // Check if we reached the limit of items
-    if ($i == $limit) {
-        // Stop the execution of this loop as we have already reached the limit
-        break;
+    /**
+     * InstagramLatestPosts constructor.
+     *
+     * @param array $config Array of properties
+     */
+    public function __construct($config = [])
+    {
+        // Initialize the essential properties
+        $this->modx         = $config['modx'];
+        $this->accountName  = $config['accountName'];
+        $this->limit        = $config['limit'];
+        $this->showVideo    = $config['showVideo'];
+        $this->imageQuality = $config['imageQuality'];
+        $this->videoQuality = $config['videoQuality'];
+        $this->innerTpl     = $config['innerTpl'];
+        $this->outerTpl     = $config['outerTpl'];
+        $this->errorTpl     = $config['errorTpl'];
     }
 
-    // Check if video is available and if it should be shown
-    if (isset($item->videos) && $showVideo) {
-        $resources[$i]['url'] = $item->videos->$videoQuality->url;
-        $resources[$i]['type'] = 'video';
-    } else {
-        // Otherwise set the image preview
-        $resources[$i]['url'] = $item->images->$imageQuality->url;
-        $resources[$i]['type'] = 'image';
+    /**
+     * Runs the data processing
+     *
+     * @return boolean $result The result of data processing
+     */
+    public function run()
+    {
+        // Check if the Instagram account name is not set
+        if ($this->accountName == '') {
+            $this->error = 'Instagram account name is required. Please set this property in your snippet call.';
+            return false;
+        }
+
+        // Get the available server method to download the remote content
+        $this->serverMethod = $this->getServerMethod();
+
+        // Check if no server method is available
+        if ($this->serverMethod === null) {
+            $this->error = 'Please enable allow_url_fopen or cURL on your web server.';
+            return false;
+        }
+
+        // Set the account URL
+        $this->accountUrl   = 'https://www.instagram.com/' . $this->accountName;
+
+        // Set the JSON URL
+        $jsonUrl = $this->accountUrl . '/media/';
+
+        // Get the JSON content
+        $json = $this->getJsonContent($jsonUrl);
+
+        // Check if loading of JSON content failed
+        if ($json === false) {
+            $this->error = 'The remote loading of JSON content failed. Please check if your account name is correct.';
+            return false;
+        }
+
+        // Parse JSON in an object
+        $data = $this->parseJson($json);
+
+        // Check if JSON parsing failed
+        if ($data === null) {
+            $this->error = 'The JSON parsing failed.';
+            return false;
+        }
+
+        // Get JSON data in an object containing resources
+        $resources = $this->getResources($data);
+
+        if (count($resources) == 0) {
+            $this->error = 'There are no posts yet in the profile: ' . $this->accountName;
+            return false;
+        }
+
+        // Set the output
+        $this->output = $this->setOutput($resources);
+
+        return true;
     }
 
-    // Set the link to the corresponding post
-    $resources[$i]['link'] = $item->link;
+    /**
+     * Gets the output
+     *
+     * @return string $output HTML output
+     */
+    public function getOutput()
+    {
+        return $this->output;
+    }
 
-    $i++;
+    /**
+     * Gets the error (if there was any)
+     *
+     * @return string Error content
+     */
+    public function getError()
+    {
+        // Check if there were no any error
+        if (!isset($this->error)) {
+            return '';
+        }
+
+        // Get the error content
+        return $this->modx->getChunk($this->errorTpl, ['error' => $this->error]);
+    }
+
+
+    /**
+     * Gets the available server method which allows to download the remote content
+     *
+     * @return mixed string | null $serverMethod The name of server method or null if both allow_url_fopen and cURL are disabled
+     */
+    protected function getServerMethod()
+    {
+        // Check if file_get_contents is enabled
+        if (ini_get('allow_url_fopen')) {
+            $serverMethod = 'fopen';
+        } // Check if cURL is enabled
+        elseif (function_exists('curl_version')) {
+            $serverMethod = 'curl';
+        }
+
+        return (isset($serverMethod)) ? $serverMethod : null;
+    }
+
+    /**
+     * Gets the JSON content
+     *
+     * @param string $url JSON url
+     * @return mixed string | false $json JSON content or false if there was some error while downloading the remote content
+     */
+    protected function getJsonContent($url = '')
+    {
+        $json = null;
+
+        // Check if file_get_contents is enabled
+        if ($this->serverMethod == 'fopen') {
+            // Get the JSON content using fopen
+            $json = $this->loadFileOpen($url);
+        } // Check if cURL is enabled
+        elseif ($this->serverMethod == 'curl') {
+            // Try to get the JSON content using cURL
+            $json = $this->loadCurl($url);
+        }
+
+        return $json;
+    }
+
+    /**
+     * Parses the JSON string in an object
+     *
+     * @param string $json JSON content
+     * @return mixed stdClass | null $data Object containing JSON data or null if the JSON processing failed
+     */
+    protected function parseJson($json = '')
+    {
+        // Decode JSON an object
+        return @json_decode($json);
+    }
+
+    /**
+     * Loads the remote content using file_get_contents()
+     *
+     * @param string $url
+     * @return mixed string | boolean
+     */
+    protected function loadFileOpen($url = '')
+    {
+        return file_get_contents($url);
+    }
+
+    /**
+     * Loads the remote content using Client URL Library (cURL)
+     *
+     * @param string $url
+     * @return mixed
+     */
+    protected function loadCurl($url = '')
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        $data = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $data;
+    }
+
+    /**
+     * Gets the resources
+     *
+     * @param stdClass $data Object containing JSON data
+     * @return array $resources Prepared array of resources
+     */
+    protected function getResources(stdClass $data)
+    {
+        // Prepare the variables
+        $resources = [];
+        $i = 0;
+        $videoQuality = $this->videoQuality;
+        $imageQuality = $this->imageQuality;
+
+        foreach ($data->items as $item) {
+            // Check if we reached the limit of items
+            if ($i == $this->limit) {
+                // Stop the execution of this loop as we have already reached the limit
+                break;
+            }
+
+            // Check if video is available and if it should be shown
+            if (isset($item->videos) && $this->showVideo) {
+                $resources[$i]['url'] = $item->videos->$videoQuality->url;
+                $resources[$i]['type'] = 'video';
+            } else {
+                // Otherwise set the image preview
+                $resources[$i]['url'] = $item->images->$imageQuality->url;
+                $resources[$i]['type'] = 'image';
+            }
+
+            // Set the link to the corresponding post
+            $resources[$i]['link'] = $item->link;
+
+            $i++;
+        }
+
+        return $resources;
+    }
+
+
+    /**
+     * Sets the output
+     *
+     * @param array $resources
+     * @return string $output
+     */
+    protected function setOutput($resources = [])
+    {
+        $items = '';
+
+        foreach ($resources as $resource) {
+            // Get the inner content
+            $items .= $this->modx->getChunk($this->innerTpl, $resource);
+        }
+
+        // Get the outer content
+        $output = $this->modx->getChunk($this->outerTpl, [
+            'accountUrl'    => $this->accountUrl,
+            'items'         => $items,
+        ]);
+
+        return $output;
+    }
+
 }
 
-foreach ($resources as $resource) {
-    // Get the inner content
-    $items .= $modx->getChunk('Instagram-Inner', $resource);
-}
+// Create config array
+$config = [
+    'modx'          => $modx,
+    'accountName'   => (isset($accountName)) ? $accountName : '',
+    'limit'         => (isset($limit)) ? $limit : 6,
+    'showVideo'     => (isset($showVideo)) ? $showVideo : 0,
+    'imageQuality'  => (isset($imageQuality)) ? $imageQuality : 'low_resolution',
+    'videoQuality'  => (isset($videoQuality)) ? $videoQuality : 'low_resolution',
+    'innerTpl'      => (isset($innerTpl)) ? $innerTpl : 'Instagram-Inner',
+    'outerTpl'      => (isset($outerTpl)) ? $outerTpl: 'Instagram-Outer',
+    'errorTpl'      => (isset($errorTpl)) ? $errorTpl: 'Instagram-Error',
+];
 
-// Get the outer content
-$output = $modx->getChunk('Instagram-Outer', [
-    'url'	=> $accountUrl,
-    'items'	=> $items,
-]);
+// Create a new InstagramLatestPosts class instance
+$inst = new InstagramLatestPosts($config);
+
+// Run the data processing
+if ($inst->run()) {
+    // Get the output if processing was successfull
+    $output = $inst->getOutput();
+} else {
+    // Get the error explaining the issue
+    $output = $inst->getError();
+}
 
 return $output;
